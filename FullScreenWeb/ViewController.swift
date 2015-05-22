a3d7c76d5d5398e86bd0ad675c1af700626eddd9
David <yoxisem544@gmail.com>
2015-05-22 14:15:55 +0800

    first

--- FullScreenWeb/ViewController.swift
+++ FullScreenWeb/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    var web: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        web = UIWebView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
+        
+//        web.autoresizesSubviews = true
+        web.scrollView.bounces = false
+//        web.scalesPageToFit = true
+
+        
+        web.delegate = self
+    
+        
+        
+        self.view.addSubview(web)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        var urlrequest = NSURLRequest(URL: NSURL(string: "https://github.com/Neson/NTUvote-UI/")!)
+//        urlrequest = NSURLRequest(URL: NSURL(string: "https://google.com")!)
+        web.loadRequest(urlrequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError) {
+        println("error \(error)")
+    }
 
 }
 
